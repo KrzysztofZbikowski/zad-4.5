19b3f358ffc293bc70980795fbb409fa19b0bea4
Zbiku <Zbiku@MSI>
2023-11-18 23:44:31 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int[] tab1 = { 2, -5, 3, 5, 0 };
+        int[] tab2 = new int[tab1.Length];
+        for (int i=0; i<tab1.Length; i++)
+        {
+            tab2[(i + 1) % tab1.Length] = tab1[i];
+        }
+        for (int i = 0; i < tab1.Length; i++)
+        {
+            Console.Write(tab2[i] + " ");
+        }
+        Console.ReadKey();
+    }
+}
